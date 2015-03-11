4512f218463e98ac51add5631ec54e1f3ea8b6b5
Santiago Montoya <santyxdz@gmail.com>
2015-03-11 18:38:40 -0500

    Creado Usuario y Migrado

--- app/models.py
+++ app/models.py
@@ -13,3 +13,24 @@ class Team(db.Model):
 
     def __repr__(self):
         return '<Team %r>' % self.username
+
+
+class User(db.Model):
+    username = db.Column(db.String(15), unique=True, primary_key=True)
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(200))
+    email = db.Column(db.String(300), unique=True)
+    password = db.Column(db.String(300))
+    photo = db.Column(db.Text)
+
+    def __init__(self, username, email, password, first_name="", last_name="", photo=""):
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.photo = photo
+        print "New User: "+self.__repr__()
+
+    def __repr__(self):
+        return "<User @"+self.username+">"
