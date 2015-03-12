3975da2cc914af3774f5646c427369c04b7f642d
Santiago Montoya <santyxdz@gmail.com>
2015-03-12 14:06:40 -0500

    Registro Funcionando, sin validaciones

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 from flask import render_template, redirect, session, url_for, request, json
-from app import models, app, db
+from app import models, app, db, rest
 
 @app.route("/main")
 @app.route('/index')
@@ -8,9 +8,28 @@ from app import models, app, db
 def home():
     return render_template('home.html')
 
-@app.route("/register")
+
+@app.route("/register", methods=['POST', 'GET'])
 def register():
+    error = None
+    if request.method == "POST":
+        users = models.User.query.filter_by(username=request.form["username"].lower()).all()
+        if len(users) > 0:
+            return "ERROR: El Nombre de Usuario ya esta Registrado"
+        else:
+            user = models.User(request.form["username"], request.form["email"], request.form["password"])
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for("home"))
     return render_template("register.html")
+
+
+@app.route("/users")
+def users():
+    users_list = models.User.query.all()
+    return render_template("users.html", users=users_list)
+
+
 @app.after_request
 def add_header(response):
     """
