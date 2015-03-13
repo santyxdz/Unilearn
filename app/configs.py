8023013252c7cb67dd79e4a0bd77a44374850c92
Santiago Suarez <suarezsantiagoperez@gmail.com>
2015-03-13 09:16:35 -0500

    Intentando con FB

--- app/configs.py
+++ app/configs.py
@@ -1,1 +1,10 @@
+from os import path
+
 secret_key = 'this_should_be_configured'
+
+BASE_DIRECTORY = path.abspath(path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = '{0}{1}'.format('sqlite:///',
+                                    path.join(BASE_DIRECTORY, 'app.db'))
+FB_APP_ID='323621904515839'
+FB_APP_SECRET='3c42b3f4b82545fb7c620f46d925bb19'
+FB_APP_NAME='Unilearn'
--- app/models.py
+++ app/models.py
@@ -1,2 +1,7 @@
 #from app import db
 from app import app
+from flask.ext.social import Social
+from flask.ext.social.datastore import SQLAlchemyConnectionDatastore
+
+app.config['SECURITY_POST_LOGIN'] = '/profile'
+#db = SQLAlchemy(app)
