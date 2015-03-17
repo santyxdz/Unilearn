cfb6141c929337c35da00c2bfd5097a7c954db85
Santiago Suarez <suarezsantiagoperez@gmail.com>
2015-03-16 23:40:48 -0500

    Twitter connected

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
-from flask import Flask
+from flask import Flask, g, url_for, request,render_template, flash, json, session
 import flask
-from flask import redirect,render_template
+from flask import redirect, render_template
 from flask_oauthlib.client import OAuth
 
 from app import models, app, configs
@@ -10,13 +10,15 @@ from app import db
 
 oauth = OAuth(app)
 twitter = oauth.remote_app('twitter',
-    base_url='https://api.twitter.com/1.1/',
-    request_token_url='https://api.twitter.com/oauth/request_token',
-    access_token_url='https://api.twitter.com/oauth/access_token',
-    authorize_url='https://api.twitter.com/oauth/authenticate',
-    consumer_key=configs.TW_APP_ID,
-    consumer_secret=configs.TW_APP_SECRET,
-)
+                           base_url=configs.tw['base_url'],
+                           request_token_url=configs.tw['request_token_url'],
+                           access_token_url=configs.tw['access_token_url'],
+                           authorize_url=configs.tw['authorize_utl'],
+                           consumer_key=configs.tw['ID'],
+                           consumer_secret=configs.tw['SECRET']
+                           )
+
+
 @app.template_global()
 def is_empty(item):
     if isinstance(item, type(None)):
@@ -30,28 +32,29 @@ def is_empty(item):
 @app.route('/index')
 @app.route('/')
 def home():
-    return flask.render_template('home.html')
+    return render_template('home.html')
 
 
 @app.route("/register", methods=['POST', 'GET'])
 def register():
     error = None
-    if flask.request.method == "POST":
+    if request.method == "POST":
         users = models.User.query.filter_by(username=flask.request.form["username"].lower()).all()
         if len(users) > 0:
             return "ERROR: El Nombre de Usuario ya esta Registrado"
         else:
-            user = models.User(flask.request.form["username"], flask.request.form["email"], flask.request.form["password"])
+            user = models.User(request.form["username"], request.form["email"],
+                               request.form["password"])
             db.session.add(user)
             db.session.commit()
-            return flask.redirect(flask.url_for("home"))
-    return flask.render_template("register.html")
+            return redirect(flask.url_for("home"))
+    return render_template("register.html")
 
 
 @app.route("/users")
 def users():
     users_list = models.User.query.all()
-    return flask.render_template("users.html", users=users_list)
+    return render_template("users.html", users=users_list)
 
 
 @app.route("/courses")
@@ -73,40 +76,50 @@ def add_header(response):
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
-    return flask.render_template('404.html'), 404
+    return render_template('404.html'), 404
+
 
 @twitter.tokengetter
 def get_twitter_token():
-    if 'twitter_oauth' in flask.session:
-        resp = flask.session['twitter_oauth']
+    if 'twitter_oauth' in session:
+        resp = session['twitter_oauth']
         return resp['oauth_token'], resp['oauth_token_secret']
 
 
 @app.before_request
 def before_request():
-    flask.g.user = None
-    if 'twitter_oauth' in flask.session:
-        flask.g.user = flask.session['twitter_oauth']
+    g.user = None
+    if 'twitter_oauth' in session:
+        g.user = session['twitter_oauth']
+
 
 @app.route('/login')
 def login():
-    callback_url = flask.url_for('oauthorized', next=flask.request.args.get('next'))
-    return twitter.authorize(callback=callback_url or flask.request.referrer or None)
+    callback_url = url_for('oauthorized', next=request.args.get('next'))
+    return twitter.authorize(callback=callback_url or request.referrer or None)
+
 
 @app.route('/logout')
 def logout():
-    flask.session.pop('twitter_oauth', None)
-    return redirect(flask.url_for('index'))
+    session.pop('twitter_oauth', None)
+    return redirect(url_for('index'))
+
 
 @app.route('/oauthorized')
 def oauthorized():
     resp = twitter.authorized_response()
     if resp is None:
-        flask.flash('You denied the request to sign in')
+        flash('You denied the request to sign in')
     else:
-        flask.session['twitter_oauth'] = resp
-    return flask.redirect(flask.url_for('success'))
+        session['twitter_oauth'] = resp
+    return redirect(url_for('yeah'))
 
-@app.route('/success')
-def success():
-    return flask.render_template("register.html")
+
+@app.route('/auth')
+def yeah():
+    resp = twitter.get('account/verify_credentials.json')
+    #user = json.load(resp.data)
+    #BETA
+    if resp.status == 200:
+        flash(resp.data)
+    return render_template("login.html")
