34f9bd21d58c16e1eca5517093f0da3c1b69fbcc
Santiago Montoya <santyxdz@gmail.com>
2015-03-18 18:38:50 -0500

    Migracion

---
+++ migrations/versions/169ff636d229_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 169ff636d229
+Revises: 166fff008844
+Create Date: 2015-03-18 18:37:44.498672
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '169ff636d229'
+down_revision = '166fff008844'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
