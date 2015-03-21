8d60871f5d8736cc2c44e94d5951f2ae11b0ba35
Santiago Montoya <santyxdz@gmail.com>
2015-03-21 13:52:09 -0500

    Migrande para agregar la tabla de clasificar

---
+++ migrations/versions/2e1c4942c247_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 2e1c4942c247
+Revises: 46b5a1643f1b
+Create Date: 2015-03-21 13:50:51.851904
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2e1c4942c247'
+down_revision = '46b5a1643f1b'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
