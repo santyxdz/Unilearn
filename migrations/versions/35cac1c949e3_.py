76b8ddddc1c1abe563657d9aa49e27edfc07eafa
Santiago Montoya <santyxdz@gmail.com>
2015-04-18 11:37:04 -0500

    Arreglando error con UserScore

---
+++ migrations/versions/35cac1c949e3_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 35cac1c949e3
+Revises: 3534ddd1883e
+Create Date: 2015-04-18 11:35:50.133445
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '35cac1c949e3'
+down_revision = '3534ddd1883e'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
