70bb419fdcc7636590d39dc878adc5d3033577d1
Santiago Montoya <santyxdz@gmail.com>
2015-05-09 09:39:48 -0500

    Migrando y Arreglando Imports

---
+++ migrations/versions/26db6ad5aac4_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 26db6ad5aac4
+Revises: 66b8ba2d1c2
+Create Date: 2015-05-09 09:38:58.667910
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '26db6ad5aac4'
+down_revision = '66b8ba2d1c2'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
