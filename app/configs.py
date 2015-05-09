74bcea47a247132412ec3eb8ab59305f6da42c48
Santiago Montoya <santyxdz@gmail.com>
2015-05-09 08:25:51 -0500

    Fixed Database URI

--- app/configs.py
+++ app/configs.py
@@ -31,8 +31,7 @@ gl = {
 }
 
 secret_key = 'Unilearn-SMS-YouWillNeverGuess'
-SQLALCHEMY_DATABASE_URI = "postgres://dojilftyeldmte:SzQMKPagyO3-Iil7PQF7zXWgM8@ec2-23-21-183-70.compute-1." + \
-                          "amazonaws.com:5432/d4h8bjpkso4a3l"
+SQLALCHEMY_DATABASE_URI = "postgres://uumeqpfnmvqyou:3_qL1AEWBrHC8XCmp5PG2bfJqF@ec2-23-21-96-129.compute-1.amazonaws.com:5432/dbtla9hjh2jsa8"
 appName = "Unilearn"
 version = "0.0.2"
 versionName = "Bulbasaur"
