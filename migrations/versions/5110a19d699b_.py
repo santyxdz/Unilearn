51a1c2a2718f4358da24d3d1d618f0c10822c277
Santiago Montoya Angarita <santyxdz@gmail.com>
2015-05-14 17:31:34 -0500

    Migrando +HelpVideos Table y +Atributo Question.title

---
+++ migrations/versions/5110a19d699b_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 5110a19d699b
+Revises: 1a9ad7d690ca
+Create Date: 2015-05-14 17:30:32.983000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '5110a19d699b'
+down_revision = '1a9ad7d690ca'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
